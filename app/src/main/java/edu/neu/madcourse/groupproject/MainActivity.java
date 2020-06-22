aac8859d2a0d5f78fd1947d221d75ffc093a075b
ElliotLiu <liu.yic@husky.neu.edu>
2020-06-22 15:15:59 -0400

    Modified Persmission and Features

--- app/src/main/java/edu/neu/madcourse/groupproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/groupproject/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.neu.madcourse.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,11 +13,15 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private static final int REQUEST_ACTIVITY_RECOGNITION = 1;
+    private String TAG = "StepCounter";
     private int stepCount;
     TextView stepCounterTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +29,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
-        sensorManager.registerListener(this, stepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) == null) {
+            Toast.makeText(this, "Null Sensor", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Null Sensor");
+        } else {
+            Sensor stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            sensorManager.registerListener(this, stepDetectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            Log.d(TAG, "Registered");
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, REQUEST_ACTIVITY_RECOGNITION);
+        }
+
 
         stepCounterTextView = findViewById(R.id.textView2);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        Log.d(TAG, "Sensor Changed");
         //Get sensor values
         switch (event.sensor.getType()) {
             case (Sensor.TYPE_STEP_DETECTOR):
-                stepCount +=  event.values[0];
-                Log.d("COUNT", Integer.toString(stepCount));
+                stepCount += event.values[0];
+                Log.d(TAG, Integer.toString(stepCount));
                 stepCounterTextView.setText(Integer.toString(stepCount));
                 break;
         }
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
