389dfef408010d6badc349d4de20b9349ee3e27b
lok23 <cheng.yang@husky.neu.edu>
2020-07-11 21:03:03 -0400

    ugly chart updated tracks step counter

--- app/src/main/java/edu/neu/madcourse/groupproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/groupproject/MainActivity.java
@@ -21,7 +21,21 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -31,9 +45,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static final int UPDATE_STEP = 2;
     private static final String INTENT_FILTER = "edu.neu.madcourse.groupproject.IntentFilter";
 
-    private int stepCount;
+    private int hour = 0; //for debugging, pretend this is the hour
+    private int stepCount = 0; //associated with stepCounterTextView
+    private List<Integer> listStepCount = new ArrayList<>(); //associated with listTextView
+
     private TextView stepCounterTextView;
     private BroadcastReceiver updateReceiver;
+    private TextView listStepCounterTextView;
+    private BarChart barChart;
+
+    private List<BarEntry> dataList;
+
 
 
     @Override
@@ -41,8 +63,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        stepCounterTextView = findViewById(R.id.textView2);
+        barChart = findViewById(R.id.mp_BarChart);
 
+        stepCounterTextView = findViewById(R.id.stepCounterTextView);
+        stepCounterTextView.setText(Integer.toString(stepCount));
+
+        listStepCounterTextView = findViewById(R.id.listStepCounterTextView);
+        listStepCounterTextView.setText(listStepCount.toString());
+
+        //private method to set up our step detector
+        setStepDetectorSensor();
+
+        //Next few lines make barchart look like a normal barchart
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        YAxis left = barChart.getAxisLeft();
+        left.setAxisMaximum(1000f);
+        left.setAxisMinimum(0f);
+        barChart.getAxisRight().setEnabled(false);
+
+        dataList = new ArrayList<>();
+        BarDataSet barDataSet1 = new BarDataSet(dataList, "Dataset 1");
+
+        BarData barData = new BarData();
+        barData.addDataSet(barDataSet1);
+
+        barChart.setData(barData);
+        barChart.invalidate();
+    }
+/*
+    private ArrayList<BarEntry> dataValues() {
+        ArrayList<BarEntry> dataVals = new ArrayList<>();
+        dataVals.add(new BarEntry(0, 3));
+        dataVals.add(new BarEntry(1, 4));
+        dataVals.add(new BarEntry(3, 6));
+        dataVals.add(new BarEntry(4, 11));
+        dataVals.add(new BarEntry(5, 11));
+        dataVals.add(new BarEntry(6, 11));
+        dataVals.add(new BarEntry(7, 11));
+        dataVals.add(new BarEntry(8, 11));
+        dataVals.add(new BarEntry(9, 11));
+        return dataVals;
+    }
+
+ */
+
+    private void setStepDetectorSensor() {
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) == null) {
@@ -56,7 +122,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, REQUEST_ACTIVITY_RECOGNITION);
         }
-
     }
 
     //SensorEventListener required method
@@ -88,7 +153,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         editor.clear();
         editor.putInt("stepCount", stepCount);
         editor.apply();
-
+        unregisterReceiver(updateReceiver);
     }
 
     @Override
@@ -100,6 +165,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         editor.clear();
         editor.putInt("stepCount", stepCount);
         editor.apply();
+        //unregisterReceiver(updateReceiver);
     }
 
     @Override
@@ -123,19 +189,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         Calendar calendar = Calendar.getInstance();
 
-        calendar.set(Calendar.HOUR_OF_DAY, 11);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 50);
+        calendar.set(Calendar.HOUR_OF_DAY, 15);
+        calendar.set(Calendar.MINUTE, 7);
+        calendar.set(Calendar.SECOND, 0);
 
         //Change the third parameter to see changes sooner
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 * 60, pendingIntent);
 
         updateReceiver=new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 Toast.makeText(context, "Group Project Toast message", Toast.LENGTH_SHORT).show();
-                stepCount = 0;
+                Log.d(TAG, "Updating listStepCount");
+
+
+                //CCHART DATA
+                dataList.add(new BarEntry(hour, stepCount));
+                BarDataSet barDataSet1 = new BarDataSet(dataList, "Dataset 1");
+
+                BarData barData = new BarData(barDataSet1);
+
+                barChart.setData(barData);
+                barChart.invalidate();
+                hour++;
+
+
+
+                listStepCount.add(stepCount);
+                listStepCounterTextView.setText(listStepCount.toString());
+                //stepCount = 0;
                 stepCounterTextView.setText(Integer.toString(stepCount));
+
             }
         };
 
