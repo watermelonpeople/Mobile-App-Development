e16d2f9eaaef93ba1165180b71ad484bcdcd8e49
lok23 <cheng.yang@husky.neu.edu>
2020-07-12 16:46:23 -0400

    Two AlarmManagers working

--- app/src/main/java/edu/neu/madcourse/groupproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/groupproject/MainActivity.java
@@ -28,25 +28,21 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
-import java.lang.reflect.Array;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private String TAG = "StepCounter";
 
     private static final int REQUEST_ACTIVITY_RECOGNITION = 1;
-    private static final int UPDATE_STEP = 2;
+    private static final int UPDATE_STEP_DAY_ID = 2;
+    private static final int UPDATE_STEP_MINUTE_ID = 3;
     private static final String INTENT_FILTER_DAY = "edu.neu.madcourse.groupproject.IntentFilterDay";
     private static final String INTENT_FILTER_MINUTE = "edu.neu.madcourse.groupproject.IntentFilterMinute";
 
-    private int hour = 0; //for debugging, pretend this is the hour
+    private int day = 0; //for debugging, pretend this is the hour
     private int stepCount = 0; //associated with stepCounterTextView
     private List<Integer> listStepCount = new ArrayList<>(); //associated with listTextView
 
@@ -175,6 +171,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
         stepCount = sharedPreferences.getInt("stepCount", 0);
+        stepCounterTextView.setText(Integer.toString(stepCount));
 
         //Move this into onCreate for actual app. This method only in onResume for testing.
         restartStatsDaily();
@@ -182,32 +179,42 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private void restartStatsDaily() {
         //THE INTENT IS HERE
-        Intent intent = new Intent(INTENT_FILTER_DAY);
-        Intent intent2 = new Intent(INTENT_FILTER_MINUTE);
+        Intent intentDay = new Intent(INTENT_FILTER_DAY);
+        Intent intentMinute = new Intent(INTENT_FILTER_MINUTE);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), UPDATE_STEP, intent, 0);
+        PendingIntent pendingIntentDay = PendingIntent.getBroadcast(getApplicationContext(), UPDATE_STEP_DAY_ID, intentDay, 0);
+        PendingIntent pendingIntentMinute = PendingIntent.getBroadcast(getApplicationContext(), UPDATE_STEP_MINUTE_ID, intentMinute, 0);
 
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        AlarmManager alarmManagerDay = (AlarmManager) getSystemService(ALARM_SERVICE);
+        AlarmManager alarmManagerMinute = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         Calendar calendar = Calendar.getInstance();
 
-        calendar.set(Calendar.HOUR_OF_DAY, 11);
-        calendar.set(Calendar.MINUTE, 38);
-        calendar.set(Calendar.SECOND, 10);
+        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        calendar.set(Calendar.MINUTE, 40);
+        calendar.set(Calendar.SECOND, 0);
 
         //Change the third parameter to see changes sooner
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 * 60, pendingIntent);
+        //Updates every 12 minutes
+        alarmManagerDay.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 * 60 * 3, pendingIntentDay);
+        //updates every 5 minutes
+        alarmManagerMinute.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 * 60, pendingIntentMinute);
 
-        updateReceiver=new BroadcastReceiver() {
+        updateReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+                //temp flipped intent_filter_minute.
+                //why won't intent_filter_minute get procced?
+                if (intent.getAction() != null) {
+                    Log.d(TAG, intent.getAction());
+                }
                 if (INTENT_FILTER_DAY.equals(intent.getAction())) {
-                    Toast.makeText(context, "Group Project Toast message", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Updating listStepCount");
+                    Toast.makeText(context, "Group Project Toast Day", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Updating today");
 
 
                     //CHART DATA
-                    dataList.add(new BarEntry(hour, stepCount));
+                    dataList.add(new BarEntry(day, stepCount));
 
                     //Removing Old Weekly Data
                     if (dataList.size() > 7) {
@@ -219,19 +226,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                     barChart.setData(barData);
                     barChart.invalidate();
-                    hour++;
+                    day++;
 
 
                     listStepCount.add(stepCount);
+                    if (listStepCount.size() > 7) {
+                        listStepCount.remove(0);
+                    }
                     listStepCounterTextView.setText(listStepCount.toString());
                     //stepCount = 0;
-                    stepCounterTextView.setText(Integer.toString(stepCount));
+                    //stepCounterTextView.setText(Integer.toString(stepCount));
+                }
+                if (INTENT_FILTER_MINUTE.equals(intent.getAction())) {
+                    Toast.makeText(context, "Group Project Toast Minute", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Updating minute");
+
+                    if (dataList.isEmpty() && listStepCount.isEmpty()) {
+                        Log.d(TAG, "Lists are empty");
+                        return;
+                    }
+
+
+                    //BarEntry barEntry = dataList.get(dataList.size() - 1);
+                    dataList.remove(dataList.size() - 1);
+                    dataList.add(new BarEntry(day, stepCount));
+
+                    BarDataSet barDataSet1 = new BarDataSet(dataList, "Dataset 1");
+
+                    BarData barData = new BarData(barDataSet1);
+
+                    barChart.setData(barData);
+                    barChart.invalidate();
+
+                    listStepCount.remove(listStepCount.size() - 1);
+                    listStepCount.add(stepCount);
+
+                    listStepCounterTextView.setText(listStepCount.toString());
                 }
             }
         };
 
         IntentFilter updateIntentFilter = new IntentFilter(INTENT_FILTER_DAY);
         registerReceiver(updateReceiver, updateIntentFilter);
+        IntentFilter updateIntentFilter2 = new IntentFilter(INTENT_FILTER_MINUTE);
+        registerReceiver(updateReceiver, updateIntentFilter2);
 
     }
 
